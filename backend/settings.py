901576680ce7caa77f0224f5628d9b29ec61a44d
AmedoFerguson <mustafaevalim9@gmail.com>
2025-02-01 15:21:10 +0200

    311

--- backend/settings.py
+++ backend/settings.py
@@ -55,7 +55,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://amedoferguson.github.io/TechnoBoom/",  # Разрешаем запросы с фронтенда, который работает на http://localhost:3000
+    "https://amedoferguson.github.io",  # Разрешаем запросы с фронтенда, который работает на http://localhost:3000
 ]
 
 # Middleware
