796f45c0ad9c63fed922d726497bcaa640f73683
AmedoFerguson <mustafaevalim9@gmail.com>
2025-02-01 14:57:11 +0200

    244

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
@@ -9,5 +10,6 @@ urlpatterns = [
     path('items/', include('items.urls')),
     path('auth/', include('users.urls')),
     path('', include('items.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
